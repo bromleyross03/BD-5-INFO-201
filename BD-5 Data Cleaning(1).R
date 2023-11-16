e2875679a289a79280db6f559b187360f61f5245
Mohaned26 <149349426+Mohaned26@users.noreply.github.com>
2023-11-15 18:19:31 -0800

    Add files via upload

---
+++ BD-5 Data Cleaning(1).R
@@ -0,0 +1,37 @@
+#library statements
+library(dplyr)
+library(stringr)
+
+#read in dataframes
+gdp_df <- read.csv("GDP by Country 1999-2022.csv")
+happiness_df <- read.csv("2022.csv")
+
+#join dataframes
+df <- merge(x = gdp_df, y = happiness_df,
+            by.x = "Country",
+            by.y = "Country",
+            all.x = TRUE
+)
+
+
+df$"X1999" <- NULL
+df$"X2000" <- NULL
+df$"X2001" <- NULL
+df$"X2002" <- NULL
+df$"X2003" <- NULL
+df$"X2004" <- NULL
+df$"X2005" <- NULL
+df$"X2006" <- NULL
+df$"X2007" <- NULL
+df$"X2008" <- NULL
+df$"X2009" <- NULL
+df$"X2010" <- NULL
+df$"X2011" <- NULL
+df$"X2012" <- NULL
+df$"X2013" <- NULL
+df$"X2014" <- NULL
+df$"X2015" <- NULL
+df$"X2016" <- NULL
+df$"X2017" <- NULL
+df$"X2018" <- NULL
+df$"X2019" <- NULL
