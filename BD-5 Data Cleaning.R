57784402ca94c3e7133085f55c25580901be2a57
bromleyross03 <146286500+bromleyross03@users.noreply.github.com>
2023-11-15 18:00:39 -0800

    Add files via upload

--- BD-5 Data Cleaning.R
+++ BD-5 Data Cleaning.R
@@ -1,1 +1,16 @@
-#
+#library statements
+library(dplyr)
+library(stringr)
+
+#read in dataframes
+gdp_df <- read.csv("~/Downloads/GDP by Country 1999-2022.csv")
+happiness_df <- read.csv("~/Downloads/2022.csv")
+
+#join dataframes
+df <- merge(x = gdp_df, y = happiness_df,
+            by.x = "Country",
+            by.y = "Country",
+            all.x = TRUE
+)
+
+new_df <- filter(df, Year >= 2020)
