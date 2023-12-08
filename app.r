060aa0d3a6f3b4e4d27efb9ecf5faa622e1acf33
DuckSalad <149345015+DuckSalad@users.noreply.github.com>
2023-12-07 17:34:46 -0800

    Add files via upload

--- app.r
+++ app.r
@@ -28,18 +28,51 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
-
-  # Render choropleth map
+  
+  country_list <- c("Albania", "Algeria", "Argentina", "Armenia", "Australia", "Austria", "Bahrain",
+                    "Bangladesh", "Belgium", "Benin", "Bolivia", "Bosnia and Herzegovina", "Brazil",
+                    "Bulgaria", "Burkina Faso", "Cambodia", "Cameroon", "Canada", "Chile", "China",
+                    "Colombia", "Costa Rica", "Croatia", "Cyprus", "Denmark", "Dominican Republic",
+                    "Ecuador", "Egypt", "El Salvador", "Estonia", "Ethiopia", "Finland", "France",
+                    "Gabon", "Georgia", "Germany", "Ghana", "Greece", "Guinea", "Mali", "Peru",
+                    "Philippines", "Hungary", "Iceland", "India", "Indonesia", "Ireland", "Israel",
+                    "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Latvia", "Lebanon",
+                    "Lithuania", "Malawi", "Malaysia", "Mauritius", "Mexico", "Moldova", "Mongolia",
+                    "Morocco", "Mozambique", "Myanmar", "Namibia", "Nepal", "Netherlands", "New Zealand",
+                    "Nicaragua", "Nigeria", "Norway", "Pakistan", "Panama", "Paraguay", "Tajikistan",
+                    "Romania", "Russia", "Saudi Arabia", "Senegal", "Serbia", "Sierra Leone", "Singapore",
+                    "Slovenia", "South Africa", "Spain", "Sri Lanka", "Sweden", "Switzerland", "Taiwan Province of China",
+                    "Thailand", "Togo", "Tunisia", "Turkey", "Uganda", "Ukraine", "United Arab Emirates",
+                    "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam",
+                    "Zambia", "Zimbabwe")
+  
+  # Render map with happiness data
   output$choroplethMap <- renderPlot({
+    # Load the world map data
     world_map <- map_data("world")
-    merged_data <- merge(world_map, df_sorted, by.x = "region", by.y = "Country", all.x = TRUE)
     
-    ggplot(merged_data, aes(x = long, y = lat, group = group, fill = Explained.by..GDP.per.capita)) +
+    # Filter out the regions based on the country_list
+    world_map_filtered <- subset(world_map, region %in% country_list)
+    
+    # Assuming df_sorted has 'Country' and 'Explained.by..GDP.per.capita'
+    # Merge world_map with df_sorted data
+    merged_data <- merge(world_map_filtered, df_sorted, by.x = "region", by.y = "Country", all.x = TRUE)
+    
+    # Base world map plot with happiness data
+    ggplot(data = merged_data, aes(x = long, y = lat, group = group, fill = Explained.by..GDP.per.capita)) +
       geom_polygon(color = "black") +
       scale_fill_gradient(low = "lightblue", high = "darkblue") +
       theme_minimal() +
       coord_fixed(1.3) +
       theme(legend.position = "bottom")
+  })
+  
+  output$scatterPlot <- renderPlot({
+    # Your scatter plot code here
+  })
+  
+  output$barGraph <- renderPlot({
+    # Your bar graph code here
   })
   
   # Render scatter plot
